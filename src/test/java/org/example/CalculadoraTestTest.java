5b3fbe328023e519034c08381f200512ef482eb2
vitor.asouza <vitor.souza@ime.eb.br>
2025-10-24 09:09:55 -0300

    exemplo

---
+++ src/test/java/org/example/CalculadoraTestTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTestTest {
+
+    @Test
+    public void testSoma()
+    {
+        CalculadoraTest calc=new CalculadoraTest();
+        assertEquals(5,calc.soma(3,2));
+    }
+
+    @Test
+    public void testSubt()
+    {
+        CalculadoraTest calc=new CalculadoraTest();
+        assertEquals(1,calc.soma(3,2));
+    }
+
+}
